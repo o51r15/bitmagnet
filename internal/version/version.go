1db85f85e82db0cee7f388894d06ac1a915e18ca
o51r15 <o51r15@protonmail.com>
2026-06-24 07:24:59 -0400

    feat(version): inject build date via ldflags, display in UI menu bar instead of v-unknown

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,3 +1,8 @@
 package version
 
+// GitTag is set at build time via -ldflags for tagged releases.
 var GitTag string
+
+// BuildTime is set at build time via -ldflags to the ISO8601 UTC timestamp
+// of the CI build. Used as the displayed version when no GitTag is present.
+var BuildTime string
